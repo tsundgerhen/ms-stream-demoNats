0017aa75bb985300e6e0f044c06cfdde54b68e08
Tsundgerhen <tergelb0528@gmail.com>
2025-03-09 23:28:11 +0800

    fix: devloyment aldaanuudig zasav

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -1,24 +1,25 @@
-import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
 // Define custom configuration
-const customConfig = defineConfig({
-  globalCss: {
-    html: {
-      background: "transparent", // Transparent background for the HTML element
-    },
-    body: {
-      background: "transparent", // Transparent background for the body
-      color: "white", // Optional: Set text color (or transparent if needed)
-    },
-    "*::placeholder": {
-      opacity: 0,
-      color: "fg.subtle", // Custom placeholder style
-    },
-    "*::selection": {
-      bg: "green.200", // Custom selection style
+const customTheme = extendTheme({
+  styles: {
+    global: {
+      html: {
+        background: "transparent", // Transparent background for the HTML element
+      },
+      body: {
+        background: "transparent", // Transparent background for the body
+        color: "white", // Optional: Set text color (or transparent if needed)
+      },
+      "*::placeholder": {
+        opacity: 0,
+        color: "fg.subtle", // Custom placeholder style
+      },
+      "*::selection": {
+        bg: "green.200", // Custom selection style
+      },
     },
   },
 });
 
-// Create the system configuration by combining base and custom config
-export const system = createSystem(defaultConfig, customConfig);
+export default customTheme;
